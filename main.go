a9016de21ac1bd671ab9249856b2401a631a1af6
neeejm <nejmzakaria@gmail.com>
2022-01-07 22:06:39 +0100

    initial commit

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	ut "image-box/utils"
+)
+
+const LINE_WIDTH = 8
+const LINE_COLOR = "#0488D0"
+
+func main() {
+	fmt.Println("__main__")
+	box := ut.Box{
+		TopRow:    0.09726668,
+		RightRow:  0.7593585,
+		BottomRow: 0.84501964,
+		LeftRow:   0.33775616,
+		LineWidth: 7.5,
+		LineColor: "#0488D0", // shade of blue
+	}
+
+	ut.DrawBox(box)
+}
---
+++ utils/bounding_box.go
@@ -0,0 +1,81 @@
+package utils
+
+import (
+	"github.com/fogleman/gg"
+)
+
+type Box struct {
+	// clarifai api bounding box coordinates
+	TopRow    float64
+	RightRow  float64
+	BottomRow float64
+	LeftRow   float64
+	LineWidth float64
+	LineColor string
+}
+
+func DrawBox(box Box) {
+	// load image from path
+	im, err := gg.LoadPNG("face.png")
+	if err != nil {
+		panic(err)
+	}
+	// get image width
+	w := im.Bounds().Size().X
+	// get image heigth
+	h := im.Bounds().Size().Y
+	// draw the canvas
+	dc := gg.NewContextForImage(im)
+	dc.DrawImage(im, w, h)
+	dc.SetHexColor(box.LineColor)
+
+	// draw left column -- start
+	x1 := float64(box.LeftRow) * float64(w)
+	y1 := float64(box.TopRow) * float64(h)
+	x2 := x1
+	y2 := float64(box.BottomRow) * float64(h)
+
+	dc.SetLineWidth(box.LineWidth)
+	dc.DrawLine(x1, y1, x2, y2)
+	dc.Stroke()
+	// draw left column -- end
+
+	// draw top column -- start
+	x1 = float64(box.LeftRow) * float64(w)
+	y1 = float64(box.TopRow) * float64(h)
+	x2 = float64(box.RightRow) * float64(w)
+	y2 = y1
+
+	dc.SetLineWidth(float64(box.LineWidth))
+	dc.DrawLine(x1, y1, x2, y2)
+	dc.Stroke()
+	// draw top column -- end
+
+	// draw right column -- start
+	x1 = float64(box.RightRow) * float64(w)
+	y1 = float64(box.TopRow) * float64(h)
+	x2 = x1
+	y2 = float64(box.BottomRow) * float64(h)
+
+	dc.SetLineWidth(box.LineWidth)
+	dc.DrawLine(x1, y1, x2, y2)
+	dc.Stroke()
+	// draw right column -- end
+
+	// draw bottom column -- start
+	x1 = float64(box.LeftRow) * float64(w)
+	y1 = float64(box.BottomRow) * float64(h)
+	x2 = float64(box.RightRow) * float64(w)
+	y2 = y1
+
+	dc.SetLineWidth(box.LineWidth)
+	dc.DrawLine(x1, y1, x2, y2)
+	dc.Stroke()
+	// draw bottom column -- end
+
+	dc.SavePNG("out.png")
+}
+
+func CropBox(box Box) {
+
+}
