281461b6e0dc727f2b184e57c5cda1e49ec78936
neeejm <nejmzakaria@gmail.com>
2022-01-13 18:47:38 +0100

    adding face cropping

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"fmt"
 
-	"github.com/neeejm/image-box/utils"
+	ut "github.com/neeejm/image-box/utils"
 )
 
 const LINE_WIDTH = 8
@@ -11,22 +11,28 @@ const LINE_COLOR = "#0488D0"
 
 func main() {
 	fmt.Println("__main__")
-	box := []utils.Box{
-		{
-			TopRow:    0.09726668,
-			RightCol:  0.7593585,
-			BottomRow: 0.84501964,
-			LeftCol:   0.33775616,
-		},
-		{
-			TopRow:    0.1,
-			RightCol:  0.5,
-			BottomRow: 0.4,
-			LeftCol:   0.7,
-		},
+	box := ut.Box{
+		TopRow:    0.09726668,
+		RightCol:  0.7593585,
+		BottomRow: 0.84501964,
+		LeftCol:   0.33775616,
 	}
+	// box := []ut.Box{
+	// 	{
+	// 		TopRow:    0.09726668,
+	// 		RightCol:  0.7593585,
+	// 		BottomRow: 0.84501964,
+	// 		LeftCol:   0.33775616,
+	// 	},
+	// 	{
+	// 		TopRow:    0.1,
+	// 		RightCol:  0.5,
+	// 		BottomRow: 0.4,
+	// 		LeftCol:   0.7,
+	// 	},
+	// }
 
-	utils.DrawBox("face.png", box)
+	ut.CropImage("face.png", box)
 
 	// ut.DrawBox("face.png", box)
 
--- utils/bounding_box.go
+++ utils/bounding_box.go
@@ -1,8 +1,11 @@
 package utils
 
 import (
+	"image"
+	"image/png"
 	"io/ioutil"
 	"log"
+	"os"
 
 	"github.com/fogleman/gg"
 	"github.com/golang/freetype/truetype"
@@ -90,8 +93,136 @@ func DrawBox(imgUrl string, box []Box) {
 	dc.SavePNG("out.png")
 }
 
-func CropBox(box Box) {
+// func CropBox(imgUrl string, box []Box) {
+// 	// load image from path
+// 	im, err := gg.LoadImage(imgUrl)
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// 	// get image width
+// 	w := im.Bounds().Size().X
+// 	// get image heigth
+// 	h := im.Bounds().Size().Y
+// 	// draw the canvas
+// 	dc := gg.NewContextForImage(im)
+// 	dc.DrawImage(im, w, h)
 
+// 	for _, b := range box {
+// 		// set Box default values
+// 		if b.LineColor == "" {
+// 			b.LineColor = "#0488D0" // shade of blue
+// 		}
+// 		if b.LineWidth == 0 {
+// 			b.LineWidth = 7.5
+// 		}
+// 		dc.SetHexColor(b.LineColor)
+
+// 		// draw left column -- start
+// 		x1 := float64(b.LeftCol) * float64(w)
+// 		y1 := float64(b.TopRow) * float64(h)
+// 		x2 := x1
+// 		y2 := float64(b.BottomRow) * float64(h)
+
+// 		dc.SetLineWidth(b.LineWidth)
+// 		dc.DrawLine(x1, y1, x2, y2)
+// 		dc.Stroke()
+// 		// draw left column -- end
+
+// 		// draw top column -- start
+// 		x1 = float64(b.LeftCol) * float64(w)
+// 		y1 = float64(b.TopRow) * float64(h)
+// 		x2 = float64(b.RightCol) * float64(w)
+// 		y2 = y1
+
+// 		dc.SetLineWidth(float64(b.LineWidth))
+// 		dc.DrawLine(x1, y1, x2, y2)
+// 		dc.Stroke()
+// 		// draw top column -- end
+
+// 		// draw right column -- start
+// 		x1 = float64(b.RightCol) * float64(w)
+// 		y1 = float64(b.TopRow) * float64(h)
+// 		x2 = x1
+// 		y2 = float64(b.BottomRow) * float64(h)
+
+// 		dc.SetLineWidth(b.LineWidth)
+// 		dc.DrawLine(x1, y1, x2, y2)
+// 		dc.Stroke()
+// 		// draw right column -- end
+
+// 		// draw bottom column -- start
+// 		x1 = float64(b.LeftCol) * float64(w)
+// 		y1 = float64(b.BottomRow) * float64(h)
+// 		x2 = float64(b.RightCol) * float64(w)
+// 		y2 = y1
+
+// 		dc.SetLineWidth(b.LineWidth)
+// 		dc.DrawLine(x1, y1, x2, y2)
+// 		dc.Stroke()
+// 		// draw bottom column -- end
+// 	}
+
+// 	dc.SavePNG("out.png")
+
+// }
+
+func readImage(name string) (image.Image, error) {
+	fd, err := os.Open(name)
+	if err != nil {
+		return nil, err
+	}
+	defer fd.Close()
+
+	// image.Decode requires that you import the right image package. We've
+	// imported "image/png", so Decode will work for png files. If we needed to
+	// decode jpeg files then we would need to import "image/jpeg".
+	//
+	// Ignored return value is image format name.
+	img, _, err := image.Decode(fd)
+	if err != nil {
+		return nil, err
+	}
+
+	return img, nil
+}
+
+// writeImage writes an Image back to the disk.
+func writeImage(img image.Image, name string) error {
+	fd, err := os.Create(name)
+	if err != nil {
+		return err
+	}
+	defer fd.Close()
+
+	return png.Encode(fd, img)
+}
+
+func CropImage(name string, box Box) {
+	im, err := readImage(name)
+	if err != nil {
+		panic(err)
+	}
+
+	// get image width
+	w := im.Bounds().Size().X
+	// get image heigth
+	h := im.Bounds().Size().Y
+	// cropping coordinates
+	crop := image.Rect(int(box.LeftCol*float64(w)), int(box.TopRow*float64(h)), int(box.RightCol*float64(w)), int(box.BottomRow*float64(h)))
+
+	type subImager interface {
+		SubImage(r image.Rectangle) image.Image
+	}
+
+	// img is an Image interface. This checks if the underlying value has a
+	// method called SubImage. If it does, then we can use SubImage to crop the
+	// image.
+	cim, ok := im.(subImager)
+	if !ok {
+		panic("not supported")
+	}
+
+	writeImage(cim.SubImage(crop), "cropped-face.png")
 }
 
 func DrawText(path string) {
